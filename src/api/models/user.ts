a1e222440301536d45b494bdf6f69cca9a61e786
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-05 23:16:59 +0530

    code updated

--- src/api/models/user.ts
+++ src/api/models/user.ts
@@ -1,14 +1,36 @@
-const mongoose = require('mongoose');
+import { Schema, model, Types } from "mongoose";
 
-const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true, 
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    },
-    password: { type: String, required: true }
+/**
+ * User interface
+ */
+export interface User {
+  _id: Types.ObjectId;
+  email: string;
+  password: string;
+}
+
+/**
+ * User schema
+ */
+const userSchema = new Schema<User>({
+  _id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+/**
+ * User model
+ */
+export default model<User>("User", userSchema);
--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,64 +1,71 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import User from "../models/user.js";
 
 const router = Router();
 
-router.post("/signup", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length >= 1) {
+/**
+ * POST /user/signup
+ */
+router.post(
+  "/signup",
+  async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+      const { email, password } = req.body as {
+        email: string;
+        password: string;
+      };
+
+      const existingUser = await User.findOne({ email }).exec();
+
+      if (existingUser) {
         return res.status(409).json({
           message: "Mail exists",
         });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err,
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash,
-            });
-            user
-              .save()
-              .then((result) => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created",
-                });
-              })
-              .catch((err) => {
-                console.log(err);
-                res.status(500).json({
-                  error: err,
-                });
-              });
-          }
-        });
       }
-    });
-});
 
-router.delete("/:userId", (req, res, next) => {
-  User.remove({ _id: req.params.userId })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "User deleted",
+      const hash = await bcrypt.hash(password, 10);
+
+      const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email,
+        password: hash,
       });
-    })
-    .catch((err) => {
-      console.log(err);
+
+      await user.save();
+
+      res.status(201).json({
+        message: "User created",
+      });
+    } catch (err) {
       res.status(500).json({
         error: err,
       });
-    });
-});
+    }
+  }
+);
+
+/**
+ * DELETE /user/:userId
+ */
+router.delete(
+  "/:userId",
+  async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+      const { userId } = req.params;
+
+      await User.deleteOne({ _id: userId }).exec();
+
+      res.status(200).json({
+        message: "User deleted",
+      });
+    } catch (err) {
+      res.status(500).json({
+        error: err,
+      });
+    }
+  }
+);
 
 export default router;
